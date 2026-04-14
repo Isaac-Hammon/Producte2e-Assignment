fb5920ba9284263a7f4cad8801a36a1760b8253e
Isaac-Hammon <isaacboland23@gmail.com>
2026-04-14 13:27:48 -0600

    Product Creation Form

--- frontend/index.js
+++ frontend/index.js
@@ -11,10 +11,43 @@ for (let i = 0; i < productsJson.length; i++) {
 
 	const name = product.name;
 	const price = product.price;
-	const inventory = product.inventoryCount;
+	const inventoryCount = product.inventoryCount;
 
 	const newLi = document.createElement("li");
-	newLi.innerText = `Name: ${name}, Price: ${price}, Inventory: ${inventory}`;
+	newLi.innerText = `Name: ${name}, Price: $${Number(price).toFixed(2)}, Inventory: ${inventoryCount}`;
 
 	productsList.appendChild(newLi);
 }
+
+const form = document.querySelector("form[name=product-creation]");
+
+form.addEventListener("submit", async (e) => {
+	e.preventDefault();
+
+	const formData = new FormData(form);
+
+	const body = {
+		name: formData.get("name"),
+		price: parseFloat(formData.get("price")),
+		inventoryCount: parseInt(formData.get("inventoryCount")),
+	};
+
+	console.log("formData");
+	console.log(body);
+
+	await fetch("http://localhost:5013/products", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(body),
+	});
+
+	// show confirmation message (needed for Cypress)
+	const confirmation = document.getElementById("confirmation");
+	if (confirmation) {
+		confirmation.style.display = "block";
+	}
+
+	location.reload();
+});
