879e8ab67f28f82a608202f81d78e17ff17c0aae
Isaac-Hammon <isaacboland23@gmail.com>
2026-04-21 14:53:36 -0600

    update

--- ProductApi/obj/Debug/net10.0/ProductApi.AssemblyInfo.cs
+++ ProductApi/obj/Debug/net10.0/ProductApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09285a35e89c7d03d262de77d3c6743de0a6f683")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87d5831caf5ec96a6ce966a6b18ca4bdda6119d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
